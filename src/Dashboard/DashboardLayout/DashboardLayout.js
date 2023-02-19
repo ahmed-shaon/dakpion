e3bb0ae4cdfcc1f766d8d2c0ad39e0c6f537b805
Iqbalhossainshuvo <mdiqbalhossain203050@gmail.com>
2023-02-19 18:21:50 +0600

    Message Field complete

---
+++ src/Dashboard/DashboardLayout/DashboardLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MessageHome from './Message/MessageHome ';
+
+const DashboardLayout = () => {
+    return (
+        <div>
+            <MessageHome />
+        </div>
+    );
+};
+
+export default DashboardLayout;
